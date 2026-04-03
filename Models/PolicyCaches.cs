ca0cba5b8d5d6a1359f859b17e451b4ee5b0cd83
Simon OHara <simon.ohara@daisycomms.co.uk>
2026-04-03 10:30:45 +0100

    Added Models\PhoneNumberRecord.cs and PolicyCaches.cs

---
+++ Models/PolicyCaches.cs
@@ -0,0 +1,21 @@
+﻿namespace TeamsVoiceWizard.Models;
+
+public record PolicyEntry(string Id, string DisplayName)
+{
+    public override string ToString() => DisplayName;
+}
+
+public record UserEntry(string ObjectId, string Upn, string DisplayName)
+{
+    public override string ToString() => $"{DisplayName} ({Upn})";
+}
+
+public class PolicyCaches
+{
+    public IReadOnlyList<PolicyEntry> DialPlans { get; set; } = [];
+    public IReadOnlyList<PolicyEntry> VoiceRoutingPolicies { get; set; } = [];
+    public IReadOnlyList<UserEntry> LicensedUsers { get; set; } = [];
+
+    public bool PoliciesLoaded => DialPlans.Count > 0 || VoiceRoutingPolicies.Count > 0;
+    public bool UsersLoaded => LicensedUsers.Count > 0;
+}
